d35bdb9ff47621c4f8c3cbb93e5f4675ea6d4061
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2025-03-17 14:38:38 +0900

    feat: 필수 문제 1 구현

--- SDHSwiftMaster/main.swift
+++ SDHSwiftMaster/main.swift
@@ -7,5 +7,18 @@
 
 import Foundation
 
-print("Hello, World!")
 
+// MARK: - 필수 문제 1
+
+let sum: (Int, Int) -> Void = { num1, num2 in
+    let sum = num1 + num2
+    print("두 수의 합은 \(sum) 입니다.")
+}
+
+sum(3, 5)
+
+func calculate(_ num1: Int, _ num2: Int, closure: (Int, Int) -> Void) {
+    closure(num1, num2)
+}
+
+calculate(2, 4, closure: sum)
